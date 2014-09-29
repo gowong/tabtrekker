1277c059cdb00fa23705dfa4d6350150afa4f029
Gordon Wong <gwong001@gmail.com>
2014-09-28 18:12:22 -0700

    Open addon settings when clicking the settings link

---
+++ data/js/buttons.js
@@ -0,0 +1,20 @@
+/* Constants */
+const SETTINGS_MSG = 'settings';
+
+//called on document ready
+$(function() {
+    //register click handlers
+    $('#settings_link').click(clickHandler(SETTINGS_MSG));
+});
+
+/**
+ * Handles a click event by sending a message of the event to the addon. 
+ */
+function clickHandler(message, event) {
+    if(arguments.length < 2) {
+        return function(event) { clickHandler(message, event) };
+    }
+    self.port.emit(message);
+    event.stopPropagation();
+    event.preventDefault();
+}
