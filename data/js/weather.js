eb46427abd45703ad3e416dc90921d47d87f7a71
Gordon Wong <gwong001@gmail.com>
2014-09-30 14:49:58 -0700

    Enable high accuracy geolocation

--- data/js/weather.js
+++ data/js/weather.js
@@ -10,6 +10,12 @@ self.port.on(WEATHER_GEOLOCATION_REQUEST_MSG, getGeolocation);
  */
  function getGeolocation() {
     if('geolocation' in navigator) {
+
+        var options = {
+          enableHighAccuracy: true,
+          timeout: 5000
+        };
+
         navigator.geolocation.getCurrentPosition(function(position) {
             //only sending coordinates because for some unknown reason,
             //sending the entire position object causes it to fail serialization
@@ -17,7 +23,9 @@ self.port.on(WEATHER_GEOLOCATION_REQUEST_MSG, getGeolocation);
                 latitude: position.coords.latitude,
                 longitude: position.coords.longitude
             });
-        });
+        }, function(err) {
+            console.warn('ERROR(' + err.code + '): ' + err.message);
+        }, options);
     } else {
         console.warn('Geolocation not supported.');
     }
