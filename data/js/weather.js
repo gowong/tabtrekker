4673344cb1e8a7f9a0dbffac723933a71a23c66a
Gordon Wong <gwong001@gmail.com>
2014-09-30 03:21:09 -0700

    Add weather functionality that retrieves and caches weather conditions from Open Weather Map

---
+++ data/js/weather.js
@@ -0,0 +1,24 @@
+/* Constants */
+const WEATHER_GEOLOCATION_REQUEST_MSG = 'weather_geolocation_request';
+const WEATHER_GEOLOCATION_RESULT_MSG = 'weather_geolocation_result';
+
+//listen for messages
+self.port.on(WEATHER_GEOLOCATION_REQUEST_MSG, getGeolocation);
+
+/**
+ * Retrieves user's geolocation and sends position to the addon.
+ */
+ function getGeolocation() {
+    if('geolocation' in navigator) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            //only sending coordinates because for some unknown reason,
+            //sending the entire position object causes it to fail serialization
+            self.port.emit(WEATHER_GEOLOCATION_RESULT_MSG, {
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            });
+        });
+    } else {
+        console.warn('Geolocation not supported.');
+    }
+ }
