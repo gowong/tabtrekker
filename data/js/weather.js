9045ed917b8e6c216deb5003e5cc621a42367f18
Gordon Wong <gwong001@gmail.com>
2014-10-01 00:31:41 -0700

    Display weather temperature and location on page

--- data/js/weather.js
+++ data/js/weather.js
@@ -1,9 +1,23 @@
 /* Constants */
+const WEATHER_MSG = 'weather';
 const WEATHER_GEOLOCATION_REQUEST_MSG = 'weather_geolocation_request';
 const WEATHER_GEOLOCATION_RESULT_MSG = 'weather_geolocation_result';
 
 //listen for messages
+self.port.on(WEATHER_MSG, displayWeather);
 self.port.on(WEATHER_GEOLOCATION_REQUEST_MSG, getGeolocation);
+
+/**
+ * Displays weather information on the page.
+ */
+function displayWeather(data) {
+    $('#weather_location').html(data.location.toUpperCase());
+    //temperature
+    var temperature = parseInt(data.temperature);
+    $('#weather_temperature').html(temperature);
+    //temperature units
+    $('#weather_temperature_units').html(data.temperatureUnits);
+}
 
 /**
  * Retrieves user's geolocation and sends position to the addon.
