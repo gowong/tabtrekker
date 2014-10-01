d5dae0b2ef8f2f1b4ddb6fdcb702b0e5742095e7
Gordon Wong <gwong001@gmail.com>
2014-10-01 03:33:43 -0700

    Display weather conditions icon

--- data/js/weather.js
+++ data/js/weather.js
@@ -11,7 +11,11 @@ self.port.on(WEATHER_GEOLOCATION_REQUEST_MSG, getGeolocation);
  * Displays weather information on the page.
  */
 function displayWeather(data) {
+    //location
     $('#weather_location').html(data.location.toUpperCase());
+    //conditions
+    var icon = getConditionsIcon(data.conditions);
+    $('#weather_temperature').attr('data-icon', icon);
     //temperature
     var temperature = parseInt(data.temperature);
     $('#weather_temperature').html(temperature);
@@ -44,3 +48,86 @@ function displayWeather(data) {
         console.warn('Geolocation not supported.');
     }
  }
+
+/**
+ * Returns the character of the icon that represents the weather conditions.
+ */
+function getConditionsIcon(conditions) {
+    if(!conditions || !conditions.length) {
+        //N/A
+        return ')';
+    }
+    //iterate through weather conditions until the first icon is found
+    var icon;
+    for(var i = 0; i < conditions.length; i++) {
+        if(conditions[i] && conditions[i].icon) {
+            icon = conditions[i].icon;
+            break;
+        }
+    }
+    //could not find an icon
+    if(!icon) {
+        //N/A
+        return ')';
+    }
+    //map icon name to character in icon font
+    switch(icon) {
+        // Day
+        //Clear sky
+        case '01d':
+            return 'B';
+        //Few clouds
+        case '02d':
+            return 'H';
+        //Scattered clouds
+        case '03d':
+            return 'N';
+        //Broken clouds
+        case '04d':
+            return 'Y';
+        //Shower rain
+        case '09d':
+            return 'Q';
+        //Rain
+        case '10d':
+            return 'R';
+        //Thunderstorm
+        case '11d':
+            return '0';
+        //Snow
+        case '13d':
+            return 'W';
+        //Mist
+        case '50d':
+            return 'M';
+
+        //Night
+        //Clear sky
+        case '01n':
+            return 'C';
+        //Few clouds
+        case '02n':
+            return 'I';
+        //Scattered clouds
+        case '03n':
+            return 'N';
+        //Broken clouds
+        case '04n':
+            return 'Y';
+        //Shower rain
+        case '09n':
+            return 'Q';
+        //Rain
+        case '10n':
+            return 'R';
+        //Thunderstorm
+        case '11n':
+            return '0';
+        //Snow
+        case '13n':
+            return 'W';
+        //Mist
+        case '50n':
+            return 'M';
+    }
+}
