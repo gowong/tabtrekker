b2d231dbbdaf4d2388bd88c7dd81c58578782e05
Gordon Wong <gwong001@gmail.com>
2014-10-18 15:42:00 -0700

    #3 Add most visited links dropdown

---
+++ data/js/history.js
@@ -0,0 +1,34 @@
+/* Constants */
+//messages
+const HISTORY_MSG = 'history';
+
+//listen for messages
+self.port.on(HISTORY_MSG, initHistory);
+
+/**
+ * Initializes history results.
+ */
+function initHistory(results) {
+    //append each result to history list
+    for(var i = 0; i < results.length; i++) {
+        appendHistoryResult(results[i]);
+    }
+}
+
+/**
+ * Appends history result to list of history results.
+ */
+function appendHistoryResult(result) {
+    var title = result.title;
+    var url = result.url;
+    var iconUrl = result.iconUri ? 
+        result.iconUri.substring('moz-anno:favicon:'.length) : '';
+
+    $('#history_list').append(
+        $('<li>').append(
+            $('<a>').attr('href', url).append(
+                $('<object>').attr('data', iconUrl).append(
+                    $('<span>').attr('class', 'glyphicon glyphicon-link'))).append(
+                $('<span>').append(title))
+    ));
+}
