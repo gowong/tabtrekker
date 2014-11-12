b45ecdc4bbf2f3d95dc30bb0da834568abdf6b41
Gordon Wong <gwong001@gmail.com>
2014-11-11 22:56:06 -0800

    Add dev and production Parse apps

---
+++ addon/lib/config.js
@@ -0,0 +1,29 @@
+/**
+ * Configurations module.
+ */
+var NewTabConfig = {
+
+    dev: true,
+
+    parseDev: {
+        appId: 'ljUR1d7dsOLJmWniAszRSCS1jl2PTUpbyyC3WBUZ',
+        restApiKey: 'P1E87oyMnnsj9KZPc8OB7hjqqaI80j2936YlthD8',
+    },
+
+    parseProd: {
+        appId: 'A4zgYNwtfhU6XySHowIXJlAkxajxl2qDBDZi6SU6',
+        restApiKey: 'tNKNYALwg66YZrec2fhzeK8aBQoJ5UZw21hmtLvm'
+    },
+
+    getParseAppId: function() {
+        return NewTabConfig.dev ? NewTabConfig.parseDev.appId
+            : NewTabConfig.parseProd.appId;
+    },
+
+    getParseRestApiKey: function() {
+        return NewTabConfig.dev ? NewTabConfig.parseDev.restApiKey
+            : NewTabConfig.parseProd.restApiKey;
+    }
+};
+
+exports.NewTabConfig = NewTabConfig;
