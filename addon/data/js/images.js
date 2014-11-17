4c601108b62158127b54a77c2112ca0ca4fe4077
Gordon Wong <gwong001@gmail.com>
2014-11-16 18:48:55 -0800

    Only load fallback image if image background fails to load

--- addon/data/js/images.js
+++ addon/data/js/images.js
@@ -13,20 +13,44 @@ var NewTabImages = {
      * Displays the image.
      */
     displayImage: function(image) {
-        //set image
-        var background = $(document.body);
         var imageSrc = image.fileUri || image.imageUrl;
-        var imageFallback = image.fallback;
-        var backgroundImage = 'url(' + imageSrc + '), url(' + imageFallback + ')';
-        background.css('background-image', backgroundImage);
+        if(imageSrc) {
+            //set image
+            var backgroundImage = 'url(' + imageSrc + ')';
+            $(document.body).css('background-image', backgroundImage);
 
-        //set image info
-        var imageSetName = image.imageSetName;
-        var imageSetLink = image.imageSetInfoUrl;
-        var imageDownloadLink = image.infoUrl;
-        $('#image_set_info').text(imageSetName);
-        $('#image_set_info').attr('href', imageSetLink);
-        $('#image_download').attr('href', imageDownloadLink);
+            //set image info
+            $('#image_set_info').text(image.imageSetName);
+            $('#image_set_info').attr('href', image.imageSetInfoUrl);
+            $('#image_download').attr('href', image.infoUrl);
+        } else {
+            NewTabImages.displayFallbackImage(image);
+        }
+
+        //detect errors when loading image, jquery docs say errors when loading
+        //local images (file://image.png) won't be detected, but it seems to be
+        //working in firefox
+        $('<img>').error(function() {
+            NewTabImages.displayFallbackImage(image);
+            $(this).remove();
+        }).attr('src', imageSrc).each(function() {
+            //fail-safe for cached images which sometimes don't trigger load events
+            if(this.complete) {
+                $(this).load();
+            }
+        });
+    },
+
+    /**
+     * Displays the fallback image.
+     */
+    displayFallbackImage: function(image) {
+        //set background image
+        var fallbackImage = 'url(' + image.fallback + ')';
+        $(document.body).css('background-image', fallbackImage);
+        //hide image info
+        $('#image_set_info').css('display', 'none');
+        $('#image_download').css('display', 'none');
     }
 };
 
