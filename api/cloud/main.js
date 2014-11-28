9b32ea168fc8a22d2be70015175600f96a681168
Gordon Wong <gwong001@gmail.com>
2014-11-28 04:04:17 -0800

    Handle invalid image set ids in api code

--- api/cloud/main.js
+++ api/cloud/main.js
@@ -9,7 +9,7 @@ var imageSets = require('cloud/imagesets.js').ImageSets;
  */
 Parse.Cloud.define('getNextImageSet', function(request, response) {
     var id = parseInt(request.params.id, 10);
-    var nextId = isNaN(id) ? 0 : (id + 1) % imageSets.length;
+    var nextId = (isNaN(id) || id < 0 || id >= imageSets.length) ? 0 : (id + 1) % imageSets.length;
     var imageSet = imageSets[nextId];
     response.success(imageSet);
 });
