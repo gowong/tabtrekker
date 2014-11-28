5d3c8d46e93ec82057b0581cf5438e4153776118
Gordon Wong <gwong001@gmail.com>
2014-11-28 01:24:11 -0800

    #20 placeholder image sets

--- api/cloud/imagesets.js
+++ api/cloud/imagesets.js
@@ -6,13 +6,205 @@
 var ImageSets = [
     {
         id: 0,
-        name: 'Test',
-        infoUrl: 'www.gordonwong.com',
+        name: 'San Francisco, USA',
+        infoUrl: 'http://www.gordonwong.com',
         images: [
             {
                 infoUrl: 'https://interfacelift.com/wallpaper/details/3750/realm_of_light.html',
                 imageUrl: 'http://newtabdev.parseapp.com/1b3E67J.jpg'
             }
+        ]
+    },
+    {
+        id: 1,
+        name: '',
+        infoUrl: '',
+        images: [
+
+        ]
+    },
+    {
+        id: 2,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 3,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 4,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 5,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 6,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 7,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 8,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 9,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 10,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 11,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 12,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 13,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 14,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 15,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 16,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 17,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 18,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 19,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 20,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 21,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 22,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 23,
+        name: '',
+        infoUrl: '',
+        images: [
+            
+        ]
+    },
+    {
+        id: 24,
+        name: '',
+        infoUrl: '',
+        images: [
+            
         ]
     }
 ];
