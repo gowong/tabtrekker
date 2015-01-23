6a84a842851830179f4d87b79586af7614f8e5b0
Gordon Wong <gwong001@gmail.com>
2015-01-22 19:11:03 -0800

    #20 finalize image sets order for v1 release

--- api/cloud/imagesets.js
+++ api/cloud/imagesets.js
@@ -6,9 +6,43 @@
  */
 exports.ImageSets = [
     //0
-    require('cloud/imagesets/atlantic_ocean_road.js').ImageSet,
+    require('cloud/imagesets/hallstatt.js').ImageSet,
     //1
-    require('cloud/imagesets/bora_bora.js').ImageSet,
+    require('cloud/imagesets/paris.js').ImageSet,
     //2
-    require('cloud/imagesets/fiordland.js').ImageSet
+    require('cloud/imagesets/fiordland.js').ImageSet,
+    //3
+    require('cloud/imagesets/new_york.js').ImageSet,
+    //4
+    require('cloud/imagesets/venice.js').ImageSet,
+    //5
+    require('cloud/imagesets/tuscany.js').ImageSet,
+    //6
+    require('cloud/imagesets/grand_canyon.js').ImageSet,
+    //7
+    require('cloud/imagesets/san_francisco.js').ImageSet,
+    //8
+    require('cloud/imagesets/neuschwanstein_castle.js').ImageSet,
+    //9
+    require('cloud/imagesets/aurora.js').ImageSet,
+    //10
+    require('cloud/imagesets/bora_bora.js').ImageSet,
+    //11
+    require('cloud/imagesets/st_petersburg.js').ImageSet,
+    //12
+    require('cloud/imagesets/meteora.js').ImageSet,
+    //13
+    require('cloud/imagesets/atlantic_ocean_road.js').ImageSet,
+    //14
+    require('cloud/imagesets/stockholm.js').ImageSet,
+    //15
+    require('cloud/imagesets/manarola.js').ImageSet,
+    //16
+    require('cloud/imagesets/yosemite.js').ImageSet,
+    //17
+    require('cloud/imagesets/great_wall_of_china.js').ImageSet,
+    //18
+    require('cloud/imagesets/rio_de_janeiro.js').ImageSet,
+    //19
+    require('cloud/imagesets/matsumoto_castle.js').ImageSet
 ];
