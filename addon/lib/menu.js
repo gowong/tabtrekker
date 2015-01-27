f0445772f968f0175db04dc18598dd0bf4ed617a
Gordon Wong <gwong001@gmail.com>
2015-01-26 17:00:39 -0800

    #37 Add visibility options for menu

--- addon/lib/menu.js
+++ addon/lib/menu.js
@@ -2,20 +2,49 @@
 
 /* SDK Modules */
 const self = require('sdk/self');
+const simplePrefs = require('sdk/simple-prefs');
 const tabs = require('sdk/tabs');
 
 /* Modules */
-const logger = require('logger').TabTrekkerLogger;
 const images = require('images').TabTrekkerImages;
+const logger = require('logger').TabTrekkerLogger;
+const utils = require('utils').TabTrekkerUtils;
+var tabtrekker; //load on initialization to ensure main module is loaded
 
 /* Constants */
-const SETTINGS_MSG = 'menu_settings';
+const HIDE_MENU_MSG = 'hide_menu';
+const MENU_MSG = 'menu';
 const NEXTIMAGE_MSG = 'menu_next_image';
+const SETTINGS_MSG = 'menu_settings';
+//preferences
+const SHOW_MENU_PREF = 'show_menu';
 
 /**
  * Menu module.
  */
 var TabTrekkerMenu = {
+
+    /**
+     * Initializes menu by sending menu options to the content scripts.
+     */
+    initMenu: function(worker) {
+        tabtrekker = require('main').TabTrekkerMain;
+
+        //don't initialize menu when it is hidden
+        var menuVisibility = simplePrefs.prefs[SHOW_MENU_PREF];
+        if(menuVisibility === 'never') {
+            utils.emit(tabtrekker.workers, worker, HIDE_MENU_MSG);
+            return;
+        }
+
+        logger.log('Initializing menu.');
+
+        var options = {}
+        options[SHOW_MENU_PREF] = menuVisibility;
+        utils.emit(tabtrekker.workers, worker, MENU_MSG, options);
+
+        TabTrekkerMenu.attachListeners(worker);
+    },
 
     /**
      * Attaches listeners to listen for button click events.
