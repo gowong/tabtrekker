50c15992a4e25d12eb553d85c30b5be4514f3a3a
Gordon Wong <gwong001@gmail.com>
2015-09-07 17:44:25 -0700

    Move API secrets out of config file

--- addon/lib/config.js
+++ addon/lib/config.js
@@ -5,27 +5,8 @@
  */
 var TabTrekkerConfig = {
 
-    dev: false,
+    dev: false
 
-    parseDev: {
-        appId: 'ljUR1d7dsOLJmWniAszRSCS1jl2PTUpbyyC3WBUZ',
-        restApiKey: 'P1E87oyMnnsj9KZPc8OB7hjqqaI80j2936YlthD8',
-    },
-
-    parseProd: {
-        appId: 'A4zgYNwtfhU6XySHowIXJlAkxajxl2qDBDZi6SU6',
-        restApiKey: 'tNKNYALwg66YZrec2fhzeK8aBQoJ5UZw21hmtLvm'
-    },
-
-    getParseAppId: function() {
-        return TabTrekkerConfig.dev ? TabTrekkerConfig.parseDev.appId
-            : TabTrekkerConfig.parseProd.appId;
-    },
-
-    getParseRestApiKey: function() {
-        return TabTrekkerConfig.dev ? TabTrekkerConfig.parseDev.restApiKey
-            : TabTrekkerConfig.parseProd.restApiKey;
-    }
 };
 
 exports.TabTrekkerConfig = TabTrekkerConfig;
