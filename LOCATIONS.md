0b727e7494702f686565064dfc5ab73e140f728a
Tom <physxonfire@aol.com>
2015-09-19 23:07:06 -0700

    even more new locations added

--- LOCATIONS.md
+++ LOCATIONS.md
@@ -14,9 +14,9 @@
 - San Francisco, USA ***(Done)***
 - Washington D.C., USA ***(Done)***
 - Cape Town, South Africa
-- Dubai, United Arab Emriates
+- Dubai, United Arab Emriates ***(Done)***
 - Toronto, Canada ***(Done)***
-- London, UK
+- London, UK ***(Done)***
 - Hong Kong, China
 - Kuala Lumpur, Malaysia
 - Sydney, Australia
@@ -46,13 +46,13 @@
 - Meteora, Greece ***(Done)***
 - Bagan, Myanmar
 - Pyramids of Giza, Egypt
-- Petra, Jordan
+- Petra, Jordan ***(Done)***
 - Chichen Itza, Mexico ***(Done)***
 - Parthenon, Greece
 - Machu Picchu, Peru
 - Taj Mahal, India
 - St. Basil's Cathedral, Moscow
-- Forbidden City, Beijing, China
+- Forbidden City, Beijing, China ***(Done)***
 - *Castles*
     - Neuschwanstein Castle, Germany ***(Done)***
     - Matsumoto Castle, Japan ***(Done)***
---
+++ api/cloud/imagesets/london.js
@@ -0,0 +1,52 @@
+'use strict';
+
+exports.ImageSet = {
+    name: 'London, United Kingdom',
+    infoUrl: 'http://en.wikipedia.org/wiki/London',
+    images: [
+        {
+            infoUrl: 'https://www.flickr.com/photos/boltron/6202272031',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/0.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/39997856@N03/8737855542',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/1.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/g4egk/4121065454',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/2.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/garymacfadyen/6817452476',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/3.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/davidedamico/16779080625',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/4.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/davidedamico/15653242682',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/5.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/neilsingapore/12038742583',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/6.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/davestraven/7970059980',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/7.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/jaggers/10545653746',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/8.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/christianreimer/14871063270',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/9.jpg'
+        }
+        {
+            infoUrl: 'https://www.flickr.com/photos/nickschooley/6758864939',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/london/10.jpg'
+        }
+    ]
+};
