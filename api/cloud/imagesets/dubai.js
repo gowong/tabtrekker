d2db7196c7b8e0b4dc64c9ed4aa6c462f5db8124
Gordon Wong <gwong001@gmail.com>
2015-09-21 21:40:06 -0700

    #56 Merge 'New locations' from zqtcao

---
+++ api/cloud/imagesets/dubai.js
@@ -0,0 +1,52 @@
+'use strict';
+
+exports.ImageSet = {
+    name: 'Dubai, United Arab Emriates',
+    infoUrl: 'http://en.wikipedia.org/wiki/Dubai',
+    images: [
+        {
+            infoUrl: 'https://www.flickr.com/photos/w4nd3rl0st/12551918245',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/0.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/kemalkestelli/16248465566',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/1.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/lutz536/14033703201',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/2.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/39138647@N05/5614220391',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/3.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/st33vo/10544603844',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/4.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/klebtahi/4691365686',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/5.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/rakphoto72/17206276999',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/6.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/edwardmusiak/13051104013',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/7.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/dubai_villa/7566100368',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/8.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/roevin/5263878756',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/9.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/paolo_rosa/5266403678',
+            imageUrl: 'http://tabtrekker.parseapp.com/images/dubai/10.jpg'
+        }
+    ]
+};
