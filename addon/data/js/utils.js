b31629504c1c72fb155c8e8e4ef8c15fddc3292d
Gordon Wong <gwong001@gmail.com>
2015-11-21 17:35:31 -0800

    Fix messages not being sent to backgrounded tabs

--- addon/data/js/utils.js
+++ addon/data/js/utils.js
@@ -1,17 +1,28 @@
 'use strict';
 
+$(function() {
+    //listen to window events
+    $(window).on('beforeunload', function() {
+        TabTrekkerUtils.loaded = false;
+    });
+});
+
 /**
  * Utils module.
  */
 var TabTrekkerUtils = {
+
+    loaded: true,
 
     /**
      * Receive messages from addon.
      */
     receiveMessage: function(cb) {
         return function(payload) {
-            if(!document.hidden) {
-                cb(payload); 
+            // Only accept the message if the page is loaded (this fixes issues
+            // where a message is received right before the page is unloaded)
+            if (TabTrekkerUtils.loaded) {
+                cb(payload);
             }
         };
     }
