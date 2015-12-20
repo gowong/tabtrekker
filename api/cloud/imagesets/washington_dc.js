066b3f942b36fc7729f131e5333825eccf66bf00
Gordon Wong <gwong001@gmail.com>
2015-12-19 16:47:45 -0800

    Update Washington DC pictures

--- api/cloud/imagesets/washington_dc.js
+++ api/cloud/imagesets/washington_dc.js
@@ -5,43 +5,43 @@ exports.ImageSet = {
     infoUrl: 'https://en.wikipedia.org/wiki/Washington,_D.C.',
     images: [
         {
-            infoUrl: 'https://www.flickr.com/photos/ncindc/2962126487',
+            infoUrl: 'https://www.flickr.com/photos/omnidirectional/7090427467',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/0.jpg'
         },
         {
-            infoUrl: 'https://www.flickr.com/photos/foxypar4/3213900514',
+            infoUrl: 'https://www.flickr.com/photos/premierehdr/10761172416',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/1.jpg'
         },
         {
-            infoUrl: 'https://www.flickr.com/photos/wallyg/3666020584',
+            infoUrl: 'https://www.flickr.com/photos/tabor-roeder/5535613157',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/2.jpg'
         },
         {
-            infoUrl: 'https://www.flickr.com/photos/iceninejon/1428443225',
+            infoUrl: 'https://www.flickr.com/photos/jeffkrause/8618358070',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/3.jpg'
         },
         {
-            infoUrl: 'https://www.flickr.com/photos/fischerfotos/7432022562',
+            infoUrl: 'https://www.flickr.com/photos/wbeem/6809889782',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/4.jpg'
         },
         {
-            infoUrl: 'https://www.flickr.com/photos/iceninejon/1560137597',
+            infoUrl: 'https://www.flickr.com/photos/vpickering/6661168967',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/5.jpg'
         },
         {
-            infoUrl: 'https://www.flickr.com/photos/lydiaxliu/16975531128',
+            infoUrl: 'https://www.flickr.com/photos/thomashawk/15323466300',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/6.jpg'
         },
         {
-            infoUrl: 'https://www.flickr.com/photos/zrfraileyphotography/7050450931',
+            infoUrl: 'https://www.flickr.com/photos/endlesssimmer/2551795077',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/7.jpg'
         },
         {
-            infoUrl: 'https://www.flickr.com/photos/wallyg/3627798184',
+            infoUrl: 'https://www.flickr.com/photos/wyntuition/522812851',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/8.jpg'
         },
         {
-            infoUrl: 'https://www.flickr.com/photos/9525555@N07/6927365789',
+            infoUrl: 'https://www.flickr.com/photos/twosevenoneonenineeightthreesevenatenzerosix/8583401867',
             imageUrl: 'http://tabtrekker.parseapp.com/images/washington_dc/9.jpg'
         }
     ]
