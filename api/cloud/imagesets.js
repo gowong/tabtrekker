043743f740d972c0f8a48784672a2a06302b2fbd
Gordon Wong <gwong001@gmail.com>
2016-07-17 19:02:31 -0700

    Use relative paths in requires for Parse cloud code

--- api/cloud/imagesets.js
+++ api/cloud/imagesets.js
@@ -6,69 +6,69 @@
  */
 exports.ImageSets = [
     //0
-    require('cloud/imagesets/hallstatt.js').ImageSet,
+    require('./imagesets/hallstatt.js').ImageSet,
     //1
-    require('cloud/imagesets/paris.js').ImageSet,
+    require('./imagesets/paris.js').ImageSet,
     //2
-    require('cloud/imagesets/fiordland.js').ImageSet,
+    require('./imagesets/fiordland.js').ImageSet,
     //3
-    require('cloud/imagesets/new_york.js').ImageSet,
+    require('./imagesets/new_york.js').ImageSet,
     //4
-    require('cloud/imagesets/venice.js').ImageSet,
+    require('./imagesets/venice.js').ImageSet,
     //5
-    require('cloud/imagesets/tuscany.js').ImageSet,
+    require('./imagesets/tuscany.js').ImageSet,
     //6
-    require('cloud/imagesets/grand_canyon.js').ImageSet,
+    require('./imagesets/grand_canyon.js').ImageSet,
     //7
-    require('cloud/imagesets/san_francisco.js').ImageSet,
+    require('./imagesets/san_francisco.js').ImageSet,
     //8
-    require('cloud/imagesets/neuschwanstein_castle.js').ImageSet,
+    require('./imagesets/neuschwanstein_castle.js').ImageSet,
     //9
-    require('cloud/imagesets/aurora.js').ImageSet,
+    require('./imagesets/aurora.js').ImageSet,
     //10
-    require('cloud/imagesets/bora_bora.js').ImageSet,
+    require('./imagesets/bora_bora.js').ImageSet,
     //11
-    require('cloud/imagesets/st_petersburg.js').ImageSet,
+    require('./imagesets/st_petersburg.js').ImageSet,
     //12
-    require('cloud/imagesets/meteora.js').ImageSet,
+    require('./imagesets/meteora.js').ImageSet,
     //13
-    require('cloud/imagesets/atlantic_ocean_road.js').ImageSet,
+    require('./imagesets/atlantic_ocean_road.js').ImageSet,
     //14
-    require('cloud/imagesets/stockholm.js').ImageSet,
+    require('./imagesets/stockholm.js').ImageSet,
     //15
-    require('cloud/imagesets/manarola.js').ImageSet,
+    require('./imagesets/manarola.js').ImageSet,
     //16
-    require('cloud/imagesets/yosemite.js').ImageSet,
+    require('./imagesets/yosemite.js').ImageSet,
     //17
-    require('cloud/imagesets/great_wall_of_china.js').ImageSet,
+    require('./imagesets/great_wall_of_china.js').ImageSet,
     //18
-    require('cloud/imagesets/rio_de_janeiro.js').ImageSet,
+    require('./imagesets/rio_de_janeiro.js').ImageSet,
     //19
-    require('cloud/imagesets/matsumoto_castle.js').ImageSet,
+    require('./imagesets/matsumoto_castle.js').ImageSet,
     //20
-    require('cloud/imagesets/washington_dc.js').ImageSet,
+    require('./imagesets/washington_dc.js').ImageSet,
     //21
-    require('cloud/imagesets/toronto.js').ImageSet,
+    require('./imagesets/toronto.js').ImageSet,
     //22
-    require('cloud/imagesets/innsbruck.js').ImageSet,
+    require('./imagesets/innsbruck.js').ImageSet,
     //23
-    require('cloud/imagesets/chichen_itza.js').ImageSet,
+    require('./imagesets/chichen_itza.js').ImageSet,
     //24
-    require('cloud/imagesets/petra.js').ImageSet,
+    require('./imagesets/petra.js').ImageSet,
     //25
-    require('cloud/imagesets/london.js').ImageSet,
+    require('./imagesets/london.js').ImageSet,
     //26
-    require('cloud/imagesets/forbidden_city.js').ImageSet,
+    require('./imagesets/forbidden_city.js').ImageSet,
     //27
-    require('cloud/imagesets/dubai.js').ImageSet,
+    require('./imagesets/dubai.js').ImageSet,
     //28
-    require('cloud/imagesets/machu_picchu.js').ImageSet,
+    require('./imagesets/machu_picchu.js').ImageSet,
     //29
-    require('cloud/imagesets/angkor_wat.js').ImageSet,
+    require('./imagesets/angkor_wat.js').ImageSet,
     //30
-    require('cloud/imagesets/faroe_islands.js').ImageSet,
+    require('./imagesets/faroe_islands.js').ImageSet,
     //31
-    require('cloud/imagesets/bagan.js').ImageSet,
+    require('./imagesets/bagan.js').ImageSet,
     //32
-    require('cloud/imagesets/reine.js').ImageSet
+    require('./imagesets/reine.js').ImageSet
 ];
