0273cb7b9a04705396e1e1af21b4829d0b3c5b80
Giacomo Lacava <g.lacava@gmail.com>
2017-02-19 20:57:36 +0000

    Bologna (#93)

---
+++ api/cloud/imagesets/bologna.js
@@ -0,0 +1,48 @@
+'use strict';
+
+exports.ImageSet = {
+    name: 'Bologna, Italy',
+    infoUrl: 'http://en.wikipedia.org/wiki/Bologna',
+    images: [
+        {
+            infoUrl: 'https://www.flickr.com/photos/robertotaddeo/25626448123',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/0.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/105105658@N03/23152514652',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/1.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/playerest/15368568725',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/2.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/milonite/20055371251',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/3.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/robertotaddeo/11821136354',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/4.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/127226743@N02/26616054891',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/5.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/105105658@N03/22780526147',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/6.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/niyantha/5197740176',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/7.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/52995564@N07/7328999436',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/8.jpg'
+        },
+        {
+            infoUrl: 'https://www.flickr.com/photos/delphinidaesy/22678309928',
+            imageUrl: 'https://app.tabtrekker.com/images/bologna/9.jpg'
+        }
+    ]
+};
